a70248aafd34585cd22786b3ccfab2523df5e512
ColinHask <colinhaskins2022@gmail.com>
2025-02-10 16:27:29 -0500

    added rendering of sample html template
    run command: flask --app app run
    
    note: must cd into flaskr to run

--- src/web/flaskr/app.py
+++ src/web/flaskr/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['GET','POST'])
 def hello():
-    return "Hello World from my Conda environment!"
+    # render sample html template
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
