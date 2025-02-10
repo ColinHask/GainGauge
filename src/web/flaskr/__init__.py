59150dd02a3505be42b0921de559a2df7da5c78a
ColinHask <colinhaskins2022@gmail.com>
2025-02-10 16:15:41 -0500

    added / modified init files file structure modifications

--- src/web/flaskr/__init__.py
+++ src/web/flaskr/__init__.py
@@ -1,16 +1,27 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
-# Create a Flask application instance
-app = Flask(__name__)
 
-# Set the configuration for SQLAlchemy.
-# For development, we use SQLite. This creates a file named 'GainGauge.db' in the root directory.
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///GainGauge.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Create a SQLAlchemy instance and tie it to the Flask app.
-db = SQLAlchemy(app)
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
 
-# Optionally, import models here so they are registered.
-from . import user
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
