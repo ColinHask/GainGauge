23cc76b82879c282c5f540e669b2419585c0aa0b
ColinHask <colinhaskins2022@gmail.com>
2025-07-12 23:43:37 -0500

    formatting

--- src/gain_gauge_testing1/lib/widgets/calorie_card.dart
+++ src/gain_gauge_testing1/lib/widgets/calorie_card.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import '../models/day_data.dart';
 
-
 /// Shows one day's nutrition info
 
 class CalorieCard extends StatelessWidget {
-  const CalorieCard({
-    super.key,
-    required this.data,
-  });
+  const CalorieCard({super.key, required this.data});
 
   final DayData data;
 
@@ -22,51 +18,65 @@ class CalorieCard extends StatelessWidget {
         padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
-            Text(" Day #${data.dayNumber} ", textScaler: TextScaler.linear(1.5),),
+            Text(
+              " Day #${data.dayNumber} ",
+              textScaler: TextScaler.linear(1.5),
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                 Column(
+                Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      for (var item in data.foodItems) FittedBox(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    for (var item in data.foodItems)
+                      FittedBox(
                         fit: BoxFit.scaleDown,
-                        child: Text("${data.foodItems.indexOf(item) + 1}) ${item.name} | Cals: ${item.calories}")
+                        child: Text(
+                          "${data.foodItems.indexOf(item) + 1}) ${item.name} | Cals: ${item.calories}",
                         ),
-                    ],
-                  ),
-                
+                      ),
+                  ],
+                ),
+
                 Column(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                     Card(
+                    Card(
                       elevation: 1,
-                      color: (data.totalCalories <= data.calorieGoal) ? Colors.greenAccent : Colors.redAccent, 
+                      color: (data.totalCalories <= data.calorieGoal)
+                          ? Colors.greenAccent
+                          : Colors.redAccent,
                       child: Padding(
                         padding: const EdgeInsets.all(10.0),
                         child: FittedBox(
                           fit: BoxFit.scaleDown,
-                          child: Text("Calories: ${data.totalCalories} ", textScaler: TextScaler.linear(1.5),)
+                          child: Text(
+                            "Calories: ${data.totalCalories} ",
+                            textScaler: TextScaler.linear(1.5),
                           ),
+                        ),
                       ),
                     ),
-                    
-                   Card(
-                  color: (data.totalProtein >= data.proteinGoal) ? Colors.greenAccent : Colors.redAccent, 
-                  elevation: 1,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: FittedBox(
-                      fit: BoxFit.scaleDown,
-                      child: Text("Protein: ${data.totalProtein} ", textScaler: TextScaler.linear(1.5),)
+
+                    Card(
+                      color: (data.totalProtein >= data.proteinGoal)
+                          ? Colors.greenAccent
+                          : Colors.redAccent,
+                      elevation: 1,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: FittedBox(
+                          fit: BoxFit.scaleDown,
+                          child: Text(
+                            "Protein: ${data.totalProtein} ",
+                            textScaler: TextScaler.linear(1.5),
+                          ),
+                        ),
                       ),
-                  ),
-                )
-                
+                    ),
                   ],
                 ),
-                
               ],
             ),
           ],
